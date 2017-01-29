c4e9f04dd89f33a064419a37c8d9b5ca1e6972ae
Faisal Jeaumar <faisal.jeaumar@gmail.com>
2017-01-30 02:23:22 +0700

    Fixed build article folder

--- gulpfile.js
+++ gulpfile.js
@@ -75,6 +75,14 @@ gulp.task('fonts', () => {
     .pipe($.if(dev, gulp.dest('.tmp/fonts'), gulp.dest('dist/fonts')));
 });
 
+gulp.task('articles', () => {
+  return gulp.src([
+    'app/articles/*'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist/articles'));
+});
+
 gulp.task('extras', () => {
   return gulp.src([
     'app/*',
@@ -158,7 +166,7 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+gulp.task('build', ['lint', 'html', 'images', 'fonts', 'articles', 'extras'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
