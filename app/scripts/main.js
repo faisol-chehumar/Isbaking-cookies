090ec03a7728dbdb772e6429ec8324e104121e7c
Faisal Jeaumar <faisal.jeaumar@gmail.com>
2017-02-23 23:08:51 +0700

    Add font awesome loader script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
 console.log('\'Allo \'Allo!');
+
+(function() {
+  // Load Font Awesome script avoid block render
+  var css = document.createElement('link');
+  
+  css.href = '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css';
+  css.rel = 'stylesheet';
+  css.type = 'text/css';
+  document.getElementsByTagName('head')[0].appendChild(css);
+})();
